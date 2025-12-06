c56ef32ff0c0b609be53e83c817135fd3e036d5f
keshuanec <jirkahavlas@gmail.com>
2025-12-06 11:02:03 +0100

    lead detail setup

--- leads/urls.py
+++ leads/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.my_leads, name="my_leads"),
     path('lead/new/', views.lead_create, name='lead_create'),
     path("lead/<int:pk>/", views.lead_detail, name="lead_detail"),
+    path("lead/<int:pk>/edit/", views.lead_edit, name="lead_edit"),
     path("deals/", views.deals_list, name="deals_list"),
     path("referrers/", views.referrers_list, name="referrers_list"),
 ]
