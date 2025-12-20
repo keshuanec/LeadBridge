3a909db15274e34fb962c6241d11014535b0e32c
keshuanec <jirkahavlas@gmail.com>
2025-12-20 15:10:21 +0100

    login/logout reverse lazy

--- leads/admin.py
+++ leads/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Lead
+from .models import Lead, Deal
 
 
 @admin.register(Lead)
@@ -14,3 +14,21 @@ class LeadAdmin(admin.ModelAdmin):
     )
     list_filter = ("communication_status", "commission_status", "created_at")
     search_fields = ("client_name", "client_phone", "client_email")
+
+@admin.register(Deal)
+class DealAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "client_name",
+        "status",
+        "commission_status",
+        "loan_amount",
+        "bank",
+        "property_type",
+        "created_at",
+        "lead",
+    )
+    list_filter = ("status", "commission_status", "bank", "property_type")
+    search_fields = ("client_name", "client_phone", "client_email", "lead__client_name")
+    ordering = ("-created_at",)
+    autocomplete_fields = ("lead",)  # funguje, pokud je lead FK/O2O
